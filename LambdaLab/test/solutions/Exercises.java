31904ddcf1ddfbb3e533238b848408ea95f85716
Stuart Marks <stuart.marks@oracle.com>
2014-10-30 13:51:36 -0700

    Corresponding fix to exercise 23 for solutions.

--- LambdaLab/test/solutions/Exercises.java
+++ LambdaLab/test/solutions/Exercises.java
@@ -756,7 +756,7 @@ public class Exercises {
      */
     @Test
     public void ex23_wordFrequencies() throws IOException {
-        //UNCOMMENT//Map<Integer, Long> result = null; // TODO
+        //UNCOMMENT//Map<String, Long> result = null; // TODO
         //BEGINREMOVE
         Map<String, Long> result =
             reader.lines()
