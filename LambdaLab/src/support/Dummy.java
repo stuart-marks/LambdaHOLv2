5f9c9c352f4565dc66fbaf3159b179b952534c5f
Stuart Marks <stuart.marks@oracle.com>
2016-08-30 16:42:40 -0700

    Update README; provide README.html; fiddle with NetBeans.

---
+++ LambdaLab/src/support/Dummy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package support;
+
+/**
+ * Dummy main class; does nothing, but enables tests to run.
+ */
+public class Dummy {
+    public static void main(String[] args) {
+    }
+}
