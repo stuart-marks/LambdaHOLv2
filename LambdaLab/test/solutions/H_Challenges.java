203a41a8bb78b80dbfc77b33adb8dc18775b7c25
Stuart Marks <stuart.marks@oracle.com>
2017-09-11 23:18:22 -0700

    Rename G_Challenges to H_Challenges.

--- LambdaLab/test/solutions/H_Challenges.java
+++ LambdaLab/test/solutions/H_Challenges.java
@@ -1,4 +1,4 @@
-package exercises;
+package solutions;
 
 import java.io.Serializable;
 import java.lang.reflect.Modifier;
@@ -32,7 +32,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class G_Challenges {
+public class H_Challenges {
 
     /**
      * Denormalize this map. The input is a map whose keys are the number of legs of an animal
@@ -57,8 +57,8 @@ public class G_Challenges {
      *     ...
      *   ]
      */
-    @Test @Ignore
-    public void g1_denormalizeMap() {
+    @Test
+    public void h1_denormalizeMap() {
         Map<Integer, List<String>> input = new HashMap<>();
         input.put(4, Arrays.asList("ibex", "hedgehog", "wombat"));
         input.put(6, Arrays.asList("ant", "beetle", "cricket"));
@@ -66,7 +66,29 @@ public class G_Challenges {
         input.put(10, Arrays.asList("crab", "lobster", "scorpion"));
         input.put(750, Arrays.asList("millipede"));
 
-        List<String> result = null; // TODO
+        //TODO//List<String> result = null;
+        //BEGINREMOVE
+
+        // Simple solution: use Map.forEach to iterate over each entry,
+        // and use a nested List.forEach to iterate over each list entry,
+        // and accumulate values into the result list.
+
+        List<String> result = new ArrayList<>();
+        input.forEach((legs, names) ->
+                          names.forEach(name -> result.add(name + ":" + legs)));
+
+        // Alternative solution: stream over map entries, and use flatMap to generate
+        // Animal instances for each animal name with the given number of legs. This
+        // is more complicated, but it's a more general technique, and it can be run
+        // in parallel.
+
+//        List<String> result =
+//            input.entrySet().stream()
+//                 .flatMap(entry -> entry.getValue().stream()
+//                                        .map(name -> name + ":" + entry.getKey()))
+//                 .collect(toList());
+
+        //ENDREMOVE
 
         assertEquals(13, result.size());
         assertTrue(result.contains("ibex:4"));
@@ -121,8 +143,8 @@ public class G_Challenges {
      * pipeline (not counting nested streams), that is, in a single pass
      * over the input, without storing anything in a temporary collection.
      */
-    @Test @Ignore
-    public void g2_invertMultiMap() {
+    @Test
+    public void h2_invertMultiMap() {
         Map<String, Set<Integer>> input = new HashMap<>();
         input.put("a", new HashSet<>(Arrays.asList(1, 2)));
         input.put("b", new HashSet<>(Arrays.asList(2, 3)));
@@ -131,7 +153,28 @@ public class G_Challenges {
         input.put("e", new HashSet<>(Arrays.asList(2, 4)));
         input.put("f", new HashSet<>(Arrays.asList(3, 4)));
 
-        Map<Integer, Set<String>> result = null; // TODO
+        //TODO//Map<Integer, Set<String>> result = null;
+        //BEGINREMOVE
+        Map<Integer, Set<String>> result =
+            input.entrySet().stream()
+                 .flatMap(e -> e.getValue().stream()
+                                .map(v -> new AbstractMap.SimpleEntry<>(e.getKey(), v)))
+                 .collect(Collectors.groupingBy(Map.Entry::getValue,
+                                                Collectors.mapping(Map.Entry::getKey,
+                                                                   Collectors.toSet())));
+
+        // Alternatively:
+        // Map<Integer, Set<String>> result =
+        //     input.entrySet().stream()
+        //          .flatMap(e -> e.getValue().stream()
+        //                         .map(v -> new AbstractMap.SimpleEntry<>(v, e.getKey())))
+        //          .collect(Collectors.toMap(
+        //                       e -> e.getKey(),
+        //                       e -> new HashSet<>(Arrays.asList(e.getValue())),
+        //                       (set1, set2) -> { set1.addAll(set2); return set1; }
+        //                 ));
+
+        //ENDREMOVE
 
         assertEquals(new HashSet<>(Arrays.asList("a", "c", "d")), result.get(1));
         assertEquals(new HashSet<>(Arrays.asList("a", "b", "e")), result.get(2));
@@ -159,15 +202,19 @@ public class G_Challenges {
      * The type of the input is a Stream, so you cannot access elements at random.
      * The stream is run in parallel, so the combiner function must be correct.
      */
-    @Test @Ignore
-    public void g3_selectLongestWordsOnePass() {
+    @Test
+    public void h3_selectLongestWordsOnePass() {
         Stream<String> input = Stream.of(
             "alfa", "bravo", "charlie", "delta",
             "echo", "foxtrot", "golf", "hotel").parallel();
 
+        //UNCOMMENT//List<String> result = input.collect(
+        //UNCOMMENT//    Collector.of(null, null, null, null));
+        //UNCOMMENT//// TODO implement a collector by replacing the nulls above
+        //BEGINREMOVE
         List<String> result = input.collect(
-            Collector.of(null, null, null, null));
-        // TODO implement a collector by replacing the nulls above
+            Collector.of(Longest::new, Longest::acc, Longest::comb, Longest::finish));
+        //ENDREMOVE
 
         assertEquals(Arrays.asList("charlie", "foxtrot"), result);
     }
@@ -178,6 +225,38 @@ public class G_Challenges {
     // references to these functions to the Collector.of() method.
     // </editor-fold>
 
+    //BEGINREMOVE
+    static class Longest {
+        int len = -1;
+        List<String> list = new ArrayList<>();
+
+        void acc(String s) {
+            int slen = s.length();
+            if (slen == len) {
+                list.add(s);
+            } else if (slen > len) {
+                len = slen;
+                list.clear();
+                list.add(s);
+            } // ignore input string if slen < len
+        }
+
+        Longest comb(Longest other) {
+            if (this.len > other.len) {
+                return this;
+            } else if (this.len < other.len) {
+                return other;
+            } else {
+                this.list.addAll(other.list);
+                return this;
+            }
+        }
+
+        List<String> finish() {
+            return list;
+        }
+    }
+    //ENDREMOVE
 
 
     /**
@@ -186,11 +265,23 @@ public class G_Challenges {
      * similar to Python's itertools.groupby function, but it differs
      * from Java's Collectors.groupingBy() collector.
      */
-    @Test @Ignore
-    public void g4_splitCharacterRuns() {
+    @Test
+    public void h4_splitCharacterRuns() {
         String input = "aaaaabbccccdeeeeeeaaafff";
 
-        List<String> result = null; // TODO
+        //TODO//List<String> result = null;
+        //BEGINREMOVE
+        int[] bounds =
+            IntStream.rangeClosed(0, input.length())
+                     .filter(i -> i == 0 || i == input.length() ||
+                                  input.charAt(i-1) != input.charAt(i))
+                     .toArray();
+
+        List<String> result =
+            IntStream.range(1, bounds.length)
+                     .mapToObj(i -> input.substring(bounds[i-1], bounds[i]))
+                     .collect(Collectors.toList());
+        //ENDREMOVE
 
         assertEquals("[aaaaa, bb, cccc, d, eeeeee, aaa, fff]", result.toString());
     }
@@ -206,14 +297,20 @@ public class G_Challenges {
      * Since the stream is parallel, you MUST write a proper combiner function in order to get
      * the correct result.
      */
-    @Test @Ignore
-    public void g5_reversingCollector() {
+    @Test
+    public void h5_reversingCollector() {
         Stream<String> input =
             IntStream.range(0, 100).mapToObj(String::valueOf).parallel();
 
+        //UNCOMMENT//Collection<String> result =
+        //UNCOMMENT//    input.collect(Collector.of(null, null, null));
+        //UNCOMMENT//    // TODO fill in collector functions above
+        //BEGINREMOVE
         Collection<String> result =
-            input.collect(Collector.of(null, null, null));
-            // TODO fill in collector functions above
+            input.collect(Collector.of(ArrayDeque::new,
+                                       ArrayDeque::addFirst,
+                                       (d1, d2) -> { d2.addAll(d1); return d2; }));
+        //ENDREMOVE
 
         assertEquals(
             IntStream.range(0, 100)
@@ -244,7 +341,19 @@ public class G_Challenges {
      */
 
     OptionalInt majority(int[] array) {
-        return null; // TODO
+        //TODO//return null;
+        //BEGINREMOVE
+        Map<Integer, Long> map =
+            Arrays.stream(array)
+                  .boxed()
+                  .collect(Collectors.groupingBy(x -> x,
+                                                 Collectors.counting()));
+
+        return map.entrySet().stream()
+                  .filter(e -> e.getValue() > array.length / 2)
+                  .mapToInt(Map.Entry::getKey)
+                  .findAny();
+        //ENDREMOVE
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
@@ -252,8 +361,8 @@ public class G_Challenges {
     // and a majority-finding pass.
     // </editor-fold>
 
-    @Test @Ignore
-    public void g6_majority() {
+    @Test
+    public void h6_majority() {
         int[] array1 = { 13, 13, 24, 35, 24, 24, 35, 24, 24 };
         int[] array2 = { 13, 13, 24, 35, 24, 24, 35, 24 };
 
@@ -275,7 +384,10 @@ public class G_Challenges {
      * programming concept called "partial application."
      */
     Supplier<Shoe> makeShoeSupplier(IntFunction<Shoe> ifunc, int size) {
-        return null; // TODO
+        //TODO//return null;
+        //BEGINREMOVE
+        return () -> ifunc.apply(size);
+        //ENDREMOVE
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
@@ -292,8 +404,8 @@ public class G_Challenges {
         }
     }
 
-    @Test @Ignore
-    public void g7_shoemaker() {
+    @Test
+    public void h7_shoemaker() {
         Supplier<Shoe> sup1 = makeShoeSupplier(Shoe::new, 9);
         Supplier<Shoe> sup2 = makeShoeSupplier(Shoe::new, 13);
 
@@ -316,11 +428,25 @@ public class G_Challenges {
      * create a map with two boolean keys, true is associated to the interfaces
      * and false with the concrete classes.
      */
-    @Test @Ignore
-    public void g8_mapOfClassesAndInterfaces() {
+    @Test
+    public void h8_mapOfClassesAndInterfaces() {
 
         Class<?> origin = ArrayList.class;
-        Map<Boolean, Set<Class<?>>> result = null; // TODO
+        //TODO//Map<Boolean, Set<Class<?>>> result = null;
+        //BEGINREMOVE
+        Stream<Class<?>> classesAndInterfaces =
+            Stream.<Class<?>>iterate(origin, Class::getSuperclass)
+                  .takeWhile(Objects::nonNull)
+                  .flatMap(c -> Stream.of(Stream.of(c), Arrays.stream(c.getInterfaces())))
+                  .flatMap(Function.identity());
+
+        Predicate<Class<?>> isConcrete = c -> ! Modifier.isAbstract(c.getModifiers());
+        Predicate<Class<?>> isInterface = Class::isInterface;
+
+        Map<Boolean, Set<Class<?>>> result =
+            classesAndInterfaces.filter(isInterface.or(isConcrete))
+                                .collect(Collectors.partitioningBy(isInterface, Collectors.toSet()));
+        //ENDREMOVE
 
         assertEquals(Map.of(false, Set.of(ArrayList.class, Object.class),
                             true,  Set.of(List.class, RandomAccess.class, Cloneable.class,
@@ -345,11 +471,48 @@ public class G_Challenges {
      * and false with the concrete classes. Do that for the provided classes, and
      * arrange the result in a Map<Class, ...> with those classes as the keys.
      */
-    @Test @Ignore
-    public void g9_mapOfMapsOfClassesAndInterfaces() {
+    @Test
+    public void h9_mapOfMapsOfClassesAndInterfaces() {
 
         List<Class<?>> origin = List.of(ArrayList.class, HashSet.class, LinkedHashSet.class);
-        Map<Class<?>, Map<Boolean, Set<Class<?>>>> result = null; // TODO
+        //TODO//Map<Class<?>, Map<Boolean, Set<Class<?>>>> result = null;
+        //BEGINREMOVE
+        Function<Class<?>, Stream<Class<?>>> superClasses =
+                clazz -> Stream.<Class<?>>iterate(clazz, Class::getSuperclass)
+                               .takeWhile(Objects::nonNull);
+
+        Function<Stream<? extends Class<?>>, Stream<? extends Class<?>>> classAndInterfaces =
+                stream -> stream.flatMap(clazz -> Stream.of(Stream.of(clazz), Arrays.stream(clazz.getInterfaces())))
+                                .flatMap(Function.identity());
+
+        Function<Class<?>, Stream<? extends Class<?>>> superClassesAndInterfaces = superClasses.andThen(classAndInterfaces);
+
+        Predicate<Class<?>> isConcrete = c -> ! Modifier.isAbstract(c.getModifiers());
+        Predicate<Class<?>> isInterface = Class::isInterface;
+        Predicate<Class<?>> isInterfaceOrConcreteClass = isInterface.or(isConcrete);
+
+        // 1) To understand the algorithm, write out the previous processing as a stream pattern.
+        //    This isn't used directly, but will be converted to a collector below.
+        Map<Boolean, Set<Class<?>>> unusedResult =
+            origin.stream()
+                  .flatMap(superClassesAndInterfaces)
+                  .filter(isInterfaceOrConcreteClass)
+                  .collect(Collectors.partitioningBy(isInterface,
+                                                     Collectors.toSet()));
+
+        // 2) Convert the processing to a collector
+        Collector<Class<?>, ?, Map<Boolean, Set<Class<?>>>> collector =
+            Collectors.flatMapping(superClassesAndInterfaces,
+                                   Collectors.filtering(isInterfaceOrConcreteClass,
+                                                        Collectors.partitioningBy(isInterface,
+                                                                                  Collectors.toSet())));
+
+        // 3) use it as a downstream collector
+        Map<Class<?>, Map<Boolean, Set<Class<?>>>> result =
+            origin.stream()
+                  .collect(Collectors.groupingBy(Function.identity(),
+                                                 collector));
+        //ENDREMOVE
 
         assertEquals(
             Map.of(
